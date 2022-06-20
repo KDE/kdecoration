596e07938a77331eacb158e64be83d8a293b5806
Laurent Montel <montel@kde.org>
2022-06-20 13:33:59 +0200

    Remove duplicate headers between cpp/h

--- src/decoratedclient.cpp
+++ src/decoratedclient.cpp
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
  */
 #include "decoratedclient.h"
-#include "decoration.h"
 #include "private/decoratedclientprivate.h"
 #include "private/decorationbridge.h"
 
