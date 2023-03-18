979e4f3e1751a28b94624473e5a5fb2a3f00b20c
Xaver Hugl <xaver.hugl@gmail.com>
2023-03-18 23:56:29 +0100

    remove unnecessary std::move
    It prevents return value optimization

--- autotests/mockbridge.cpp
+++ autotests/mockbridge.cpp
@@ -10,14 +10,14 @@
 
 std::unique_ptr<KDecoration2::DecoratedClientPrivate> MockBridge::createClient(KDecoration2::DecoratedClient *client, KDecoration2::Decoration *decoration)
 {
-    auto ptr = std::unique_ptr<MockClient>(new MockClient(client, decoration));
+    auto ptr = std::make_unique<MockClient>(client, decoration);
     m_lastCreatedClient = ptr.get();
-    return std::move(ptr);
+    return ptr;
 }
 
 std::unique_ptr<KDecoration2::DecorationSettingsPrivate> MockBridge::settings(KDecoration2::DecorationSettings *parent)
 {
-    auto ptr = std::unique_ptr<MockSettings>(new MockSettings(parent));
+    auto ptr = std::make_unique<MockSettings>(parent);
     m_lastCreatedSettings = ptr.get();
-    return std::move(ptr);
+    return ptr;
 }
