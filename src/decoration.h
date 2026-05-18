a738714721e4c48c019de153cc79cf53f82f68fb
Igor Mironchik <igor.mironchik@gmail.com>
2026-05-18 10:50:47 +0300

    Fix spaceship operator

--- src/decoration.h
+++ src/decoration.h
@@ -42,7 +42,7 @@ public:
     explicit BorderRadius(qreal radius);
     explicit BorderRadius(qreal topLeft, qreal topRight, qreal bottomRight, qreal bottomLeft);
 
-    bool operator<=>(const BorderRadius &other) const = default;
+    auto operator<=>(const BorderRadius &other) const = default;
 
     qreal topLeft() const;
     qreal topRight() const;
